b2bb51b2dfc26aece1fc267d350b03743b88d2cf
AndresYepesM <aymoreno567@gmail.com>
2022-08-10 14:22:07 -0400

    Token finished

--- flightApp/models.py
+++ flightApp/models.py
@@ -29,3 +29,9 @@ class Passenger(models.Model):
 class Reservation(models.Model):
     fligth = models.ForeignKey(Flight, on_delete=models.CASCADE)
     passenger = models.OneToOneField(Passenger, on_delete=models.CASCADE)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_authtoken(sender, instance, created, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
